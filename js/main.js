917510ba3e5126f02b7c842137101bca88df1c45
veryfatwombat <veryfatwombat@gmail.com>
2021-03-26 10:17:19 +1100

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-const formSearch = document.querySelector(".form-search");
+const formSearch = document.querySelector(".form-search"),
+  inputCitiesFrom = document.querySelector(".input__cities-from"),
+  dropdownCitiesFrom = document.querySelector(".dropdown__cities-from"),
+  inputCitiesTo = document.querySelector(".input__cities-to"),
+  dropdownCitiesTo = document.querySelector(".dropdown__cities-to");
 
-alert(formSearch);
+const obj = {
+  key: "value",
+  "key two": "value2",
+  key3: true,
+  key4: {
+    a: 1,
+    b: "two",
+  },
+  keyNext: ["array", 5, { a: 1, b: 2 }, [true, false, 0]],
+};
+
+let a = 100;
+//let a = true;
+//var a = 100;
+let b = a + "1";
+
+console.log(a);
+console.log(typeof a);
+
+console.log(b);
+console.log(typeof b);
+
+let value = true;
+console.log(typeof value); // boolean
