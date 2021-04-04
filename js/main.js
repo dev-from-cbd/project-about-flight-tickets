342a633894ef2b3ba352ffe837b19d7f3d08243f
veryfatwombat <veryfatwombat@gmail.com>
2021-04-04 10:21:26 +1000

    js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const formSearch = document.querySelector(".form-search"),
   inputCitiesFrom = formSearch.querySelector(".input__cities-from"),
   dropdownCitiesFrom = formSearch.querySelector(".dropdown__cities-from"),
   inputCitiesTo = formSearch.querySelector(".input__cities-to"),
-  dropdownCitiesTo = formSearch.querySelector(".dropdown__cities-to");
+  dropdownCitiesTo = formSearch.querySelector(".dropdown__cities-to"),
+  inputDateDepart = formSearch.querySelector(".input__date-depart");
+
 const city = [
   "Melbourne",
   "Sydney",
@@ -26,7 +28,6 @@ const showCity = (input, list) => {
   if (input.value !== "") {
     const filterCity = city.filter((item) => {
       const fixItem = item.toLowerCase();
-
       return fixItem.includes(input.value.toLowerCase());
     });
 
