19dc299dfe305a03d859eed1ab4c05f67e710068
veryfatwombat <veryfatwombat@gmail.com>
2021-05-21 07:13:33 +1000

    js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const formSearch = document.querySelector(".form-search"),
   dropdownCitiesFrom = formSearch.querySelector(".dropdown__cities-from"),
   inputCitiesTo = formSearch.querySelector(".input__cities-to"),
   dropdownCitiesTo = formSearch.querySelector(".dropdown__cities-to"),
-  inputDateDepart = formSearch.querySelector(".input__date-depart");
+  inputDateDepart = formSearch.querySelector(".input__date-depart"),
+  cheapestTicket = document.getElementById("cheapest-ticket"),
+  otherCheapTickets = document.getElementById("other-cheap-tickets");
 
 // data
 const CITIESAPI = "http://api.travelpayouts.com/data/en/cities.json",
@@ -59,11 +61,80 @@ const selectCity = (event, input, list) => {
   }
 };
 
+const getNameCity = (code) => {
+  const objCity = city.find((item) => item.code === code);
+  return objCity.name;
+}
+
+const getData = (date) => {
+  return new Date(date).toLocaleString("en", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+const getChanges = (n) => {
+  if (n) {
+    return n === 1 ? "with one stop" : "with two or more transfers";
+  } else {
+    return "Without transfers";
+  }
+};
+
+const createCard = (data) => {
+const ticket = document.createElement("article");
+ticket.classList.add('ticket');
+
+let deep = "";
+
+if (data) {
+  deep = `
+    <h3 class="agent">${data.gate}</h3>
+      <div class="ticket__wrapper">
+        <div class="left-side">
+          <a href="https://www.aviasales.ru/search/SVX2905KGD1" class="button button__buy">Buy for ${data.value}&dollar;</a>
+        </div>
+        <div class="right-side">
+          <div class="block-left">
+            <div class="city__from">Departure from the city
+              <span class="city__name">${getNameCity(data.origin)}</span>
+            </div>
+            <div class="date">${getDate(data.depart_date)}</div>
+          </div>
+
+          <div class="block-right">
+            <div class="changes">${getChanges(data.number_of_changes)}</div>
+            <div class="city__to">Destination city:
+              <span class="city__name">${getNameCity(data.destination)}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+} else {
+  deep = "<h3>Unfortunately, there were no tickets available for the current date.</h3>"
+}
+
+ticket.insertAdjacentHTML("afterbegin", deep)
+
+return ticket;
+};
+
 const renderCheapDay = (cheapTicket) => {
-  console.log(cheapTicket);
+  cheapestTicket.style.display = "block";
+  cheapestTicket.innerHTML = "<h2>The cheapest ticket for the selected date</h2>";
+
+  const ticket = createCard(cheapTicket[0]);
+  cheapestTicket.append(ticket);
 };
 
 const renderCheapYear = (cheapTickets) => {
+  otherCheapTickets.style.display = "block";
+  otherCheapTickets.innerHTML = "<h2>Cheapest tickets for other dates</h2>";
+
   cheapTickets.sort((a, b) => a.value - b.value);
 
   console.log(cheapTickets);
